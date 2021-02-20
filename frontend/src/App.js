c72cb690d70749a6ff461005c0d11bb786e04847
Kewpie <72084927+q-kewpie-q@users.noreply.github.com>
2021-02-21 00:14:35 +0800

    header, toggle, and lists

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from "@chakra-ui/react"
+import { Box, Flex, Button, ButtonGroup, Heading, Stack, VStack,
+  HStack, StackDivider } from "@chakra-ui/react";
+import { List, ListItem, ListIcon, OrderedList,
+    UnorderedList, MdCheckCircle, MdSettings } from "@chakra-ui/react"
+import { Center, Square, Circle } from "@chakra-ui/react"
+import { Spacer} from "@chakra-ui/react"
+import DarkModeToggle from './DarkModeToggle.js'
+import { ColorModeScript } from "@chakra-ui/react"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box bg="teal.800" px={100} pt={100} pb={50} color='white'>
+        <Heading fontSize="6xl" >COV</Heading>
+      </Box>
+      <Box textAlign="right" bg="teal.800" px={100} py={3} color='white'>
+        <DarkModeToggle/>
+      </Box>
+      <HStack spacing="20px" m={0,2} >
+      <Flex textAlign="left" w="33%"  color='gray.400' borderWidth="1px" borderColor="gray.400" borderRadius="md">
+        <UnorderedList pl="20px" py="10px">
+          <ListItem>Lorem ipsum dolor sit amet</ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </UnorderedList>
+      </Flex>
+        <Flex textAlign="left" w="33%"  color='gray.400' borderWidth="1px" borderColor="gray.400" borderRadius="md">
+        <UnorderedList pl="20px" py="10px">
+          <ListItem>Lorem ipsum dolor sit amet</ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </UnorderedList>
+        </Flex>
+        <Flex textAlign="left" w="33%"  color='gray.400' borderWidth="1px" borderColor="gray.400" borderRadius="md">
+        <UnorderedList pl="20px" py="10px">
+          <ListItem>Lorem ipsum dolor sit amet</ListItem>
+          <ListItem>Consectetur adipiscing elit</ListItem>
+          <ListItem>Integer molestie lorem at massa</ListItem>
+          <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+        </UnorderedList>
+        </Flex>
+
+      </HStack>
+
     </div>
   );
 }
-
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import theme from "./theme";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react"
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <ChakraProvider theme={theme}>
+  <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+  </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
