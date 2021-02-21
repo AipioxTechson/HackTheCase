bcbd62b85a4beec6cfd92e2bba1bd63d1d5c3620
Kewpie <72084927+q-kewpie-q@users.noreply.github.com>
2021-02-21 11:27:29 +0800

    final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,57 +6,22 @@ import { List, ListItem, ListIcon, OrderedList,
     UnorderedList, Text, Link } from "@chakra-ui/react"
 import DarkModeToggle from './DarkModeToggle.js'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
+import Chatroom from './Chatroom.js'
+import Links from './links.js'
 
 function App() {
   return (
     <div className="App">
+
       <Box bg="teal.800" px={100} pt={100} pb={50} color='white'>
         <Heading fontSize="6xl" >COVIDBrief</Heading>
       </Box>
-      <Box textAlign="right" bg="teal.800" px={100} py={3} color='white'>
+      <Box textAlign="right"mb={3} bg="teal.800" px={100} py={3} color='white'>
         <DarkModeToggle/>
       </Box>
-      <HStack justify="center" spacing="20px" m={0,3} >
-        <Flex textAlign="left" w="33%" color='gray.400' pr={10} borderWidth="1px" borderColor="gray.400" borderRadius="md">
-          <UnorderedList pl="20px" py="10px">
-            <Heading fontSize="3xl" p={2}> COVID at a Glance </Heading>
-              <ListItem><Link href="https://informationisbeautiful.net/visualizations/covid-19-coronavirus-infographic-datapack/" isExternal>
-                Data Dashboard <ExternalLinkIcon mx="2px" />
-              </Link></ListItem>
-              <ListItem><Link href="https://informationisbeautiful.net/visualizations/covid-19-coronavirus-infographic-datapack/" isExternal>
-                Riskiest Activities <ExternalLinkIcon mx="2px" />
-              </Link></ListItem>
-              <ListItem>Integer molestie lorem at massa</ListItem>
-              <ListItem>Facilisis in pretium nisl aliquet</ListItem>
-          </UnorderedList>
-        </Flex>
-        <Flex textAlign="left" w="33%" color='gray.400' pr={10} borderWidth="1px" borderColor="gray.400" borderRadius="md">
-          <UnorderedList pl="20px" py="10px">
-            <Heading fontSize="3xl" p={2}> H1 </Heading>
-              <ListItem><Link href="https://informationisbeautiful.net/visualizations/covid-19-coronavirus-infographic-datapack/" isExternal>
-                COVID Data Dashboard <ExternalLinkIcon mx="2px" />
-              </Link></ListItem>
-              <ListItem><Link href="https://informationisbeautiful.net/visualizations/covid-19-coronavirus-infographic-datapack/" isExternal>
-                Riskiest Activities <ExternalLinkIcon mx="2px" />
-              </Link></ListItem>
-              <ListItem>Integer molestie lorem at massa</ListItem>
-              <ListItem>Facilisis in pretium nisl aliquet</ListItem>
-          </UnorderedList>
-        </Flex>
-        <Flex textAlign="left" w="33%" color='gray.400' pr={10} borderWidth="1px" borderColor="gray.400" borderRadius="md">
-          <UnorderedList pl="20px" py="10px">
-            <Heading fontSize="3xl" p={2}> H1 </Heading>
-              <ListItem><Link href="https://informationisbeautiful.net/visualizations/covid-19-coronavirus-infographic-datapack/" isExternal>
-                COVID Data Dashboard <ExternalLinkIcon mx="2px" />
-              </Link></ListItem>
-              <ListItem><Link href="https://informationisbeautiful.net/visualizations/covid-19-coronavirus-infographic-datapack/" isExternal>
-                Riskiest Activities <ExternalLinkIcon mx="2px" />
-              </Link></ListItem>
-              <ListItem>Integer molestie lorem at massa</ListItem>
-              <ListItem>Facilisis in pretium nisl aliquet</ListItem>
-          </UnorderedList>
-        </Flex>
-      </HStack>
+      <Links/>
+      <Chatroom/>
+
 
 
     </div>
---
+++ frontend/src/Chatroom.js
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from 'react';
+import { Box, Flex, Button,Heading, Stack, HStack } from "@chakra-ui/react";
+import { List, ListItem, ListIcon, OrderedList,
+    UnorderedList, Text, Textarea, Input, Editable } from "@chakra-ui/react"
+
+function Chatroom() {
+  const [messages,setMessages] = useState([]) // {type: user/bot, message: message}
+  const sessionRef = useRef(null)
+  const [formText ,setFormText] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setMessages((oldMessages) => [...oldMessages,{type:'user', message: formText}])
+    setFormText('')
+    const data = {
+      text: formText,
+      ...(sessionRef.current ? {session_id: sessionRef.current} :{})
+    }
+    // fetch("http://localhost:5000/bot/request", {
+    //   method: "POST",
+    //   headers: {'Content-Type': 'application/json'},
+    //   body: JSON.stringify(data)
+    // }).then(async response => {
+    //   if (response.ok){
+    //     const result = await response.json()
+    //     setMessages((oldMessages) => [...oldMessages,{type:'bot', message: result.content}])
+    //     if (result.type === 'NEEDS_MORE'){
+    //       sessionRef.current = result.session_id
+    //     }else {
+    //       sessionRef.current = null
+    //     }
+    //   }else {
+    //     setMessages((oldMessages) => [...oldMessages,{type:'bot', message: 'I could not understand'}])
+    //     sessionRef.current = null
+    //   }
+  // })
+    setMessages((oldMessages) => [...oldMessages,{type:'bot', message: "Hi there"}])
+  }
+
+  const handleChange = (e) => setFormText(e.target.value)
+return (
+    <Flex className="chatWindow" my={3} m="auto" w="32%" color='gray.400' px={5}
+    borderWidth="1px" borderColor="gray.400" borderRadius="md" h="auto" flexDirection='column'>
+        <ul className="chat" id="chatList" list-style-type="none" >
+          {messages.map(({type, message}, index) => (
+            <div key={index}>
+              {type === 'user' ? (
+                <li className="self">
+                    <Heading fontSize="md" pt={6} pb={1} textAlign="right">User</Heading>
+                      <Box className="msg" float="right" textAlign="right" borderWidth="1px" borderColor="gray.300" borderRadius="md" p={2}>
+                        <Box className="message"> {message}</Box>
+                      </Box>
+                </li>
+              ) : (
+                <li className="other">
+                  <Heading fontSize="md"  pt={6} pb={1} textAlign="left">Bot</Heading>
+                    <Box className="msg" float="left" textAlign="left" borderWidth="1px" borderColor="gray.300" borderRadius="md" p={2}>
+                     <Box className="message"> {message} </Box>
+                    </Box>
+                </li>
+              )}
+            </div>
+          ))}
+        </ul>
+        <Box className="chatInputWrapper" mb={0} py={2} >
+          <form onSubmit={handleSubmit} bg="teal.800" m={0}>
+            <Input
+              className="textarea input"
+              type="text"
+              value={formText}
+              placeholder="Enter your message..."
+              onChange={handleChange}
+            />
+          </form>
+        </Box>
+      </Flex>
+  );
+}
+export default Chatroom;
